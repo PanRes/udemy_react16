b9e3092d85f9c5dbb4275b23a05ab92f3da031fe
pressos <p.ressos@hotmail.com>
2018-11-06 15:49:02 +0200

    Revert changes with Error handling
    This reverts commit 3e675fa

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import classes from "./Person.css";
+import "./Person.css";
+import Radium from "radium";
 
 const person = (props) => {
 
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old!</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  )
+    const style = {
+        '@media (min-width: 500px)' : {
+            width: '450px'
+        }
+    }
+
+    return(
+    	<div className="Person" style={style}>
+			<p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old!</p>
+			<p>{props.children}</p>
+			<input type="text" onChange={props.changed} value={props.name} />
+		</div>
+	)
 };
 
-export default person;
+export default Radium(person);
