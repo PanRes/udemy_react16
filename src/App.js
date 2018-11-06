f3a7f3542ad5d44b9ced2580e81efac5066ac6c3
pressos <p.ressos@hotmail.com>
2018-11-06 15:48:30 +0200

    Revert changes with Error handling
    This reverts commit 69870e3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -23,7 +22,7 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons: persons});
+    this.setState({persons: persons})
   }
 
   deletePersonHandler = (personIndex) => {
@@ -46,22 +45,16 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {
-            this.state.persons.map((person, index) => {
-              return
-                <ErrorBoundary
-                  key={person.id}
-                >
-                  <Person
-                    click={() => this.deletePersonHandler(index)}
-                    name={person.name}
-                    age={person.age}
-                    changed={(event) => this.nameChangedHandler(event, person.id)}
-                  />
-                </ErrorBoundary>;
-            })}
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
+          })}
         </div>
-      );
+      )
 
       btnClass = classes.Red;
     }
@@ -77,7 +70,7 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <h1>{React.version} Curiosity can kill you</h1>
+        <h1>Curiosity can kill you</h1>
         <p className={assignedClasses.join(' ')}>Now it is working!</p>
         <button
           className={btnClass}
